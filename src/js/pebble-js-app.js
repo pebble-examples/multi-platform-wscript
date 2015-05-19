f763ffa562d5f080edbaf224819be77029322f61
C-D-Lewis <Bonsitm@gmail.com>
2015-05-19 13:10:18 -0700

    Update JS

--- src/js/pebble-js-app.js
+++ src/js/pebble-js-app.js
@@ -1,25 +1,11 @@
-if(Pebble.getActiveWatchInfo) {
-
-  if(Pebble.getActiveWatchInfo().platform.match("basalt")) {
-
+if(Pebble.getActiveWatchInfo && Pebble.getActiveWatchInfo().platform === 'basalt') {
     // This is the Basalt platform
     Pebble.addEventListener('ready', function() {
       console.log('PebbleKit JS ready on Basalt!');
     });  
-
-  } else {
-
-    // This is the Aplite platform
-    Pebble.addEventListener('ready', function() {
-      console.log('PebbleKit JS ready on Aplite!');
-    });
-    
-  }
 } else {
-
   // This is the Aplite platform
   Pebble.addEventListener('ready', function() {
     console.log('PebbleKit JS ready on Aplite!');
   });
-
 }
