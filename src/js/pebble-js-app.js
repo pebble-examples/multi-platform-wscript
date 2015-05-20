c91a79ee350c58ecf05b6b84cf7a27890f34c1e6
C-D-Lewis <Bonsitm@gmail.com>
2015-05-20 10:27:46 -0700

    Improve JS code

--- src/js/pebble-js-app.js
+++ src/js/pebble-js-app.js
@@ -1,11 +1,9 @@
-if(Pebble.getActiveWatchInfo && Pebble.getActiveWatchInfo().platform === 'basalt') {
+Pebble.addEventListener('ready', function() {
+  if(Pebble.getActiveWatchInfo && Pebble.getActiveWatchInfo().platform === 'basalt') {
     // This is the Basalt platform
-    Pebble.addEventListener('ready', function() {
-      console.log('PebbleKit JS ready on Basalt!');
-    });  
-} else {
-  // This is the Aplite platform
-  Pebble.addEventListener('ready', function() {
+    console.log('PebbleKit JS ready on Basalt!');
+  } else {
+    // This is the Aplite platform
     console.log('PebbleKit JS ready on Aplite!');
-  });
-}
+  }
+});  
